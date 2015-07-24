05077849d50d152caa8ad6706045136a163936ed
yoji higoshi <z.yoji.higoshi@gree.net>
2015-07-24 10:34:20 +0900

    minor tweak

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ var path = require('path');
 
 var compass = require('./lib/compass');
 
-module.exports = function( compassOptions, userSettings){
+module.exports = function( options ){
 
   // default settings
   var settings = {
     processMax: 4  // of Numnber
   }
   // override to settings
-  if(userSettings!=null){
-    for(var i in userSettings) settings[i] = userSettings[i];
+  if(options!=null){
+    for(var i in options) settings[i] = options[i];
   }
 
   // from gulp src files
@@ -61,7 +61,7 @@ module.exports = function( compassOptions, userSettings){
     // create process and handling
     for(var i=0;i<processMax;i++){
       // process spawn
-      var proc = compass.compile( filesTable[i], compassOptions );
+      var proc = compass.compile( filesTable[i], options );
       // std out
       proc.stdout.on('data', function(data){
         console.log(data+'');
